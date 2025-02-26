2afd59d798d060e9a573cccd0f05a766e08803d4
devzfy <devzfy@gmail.com>
2025-02-26 19:31:17 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-red-800 text-heading">
+        Ваш молниеносный партнер по доставке
+      </h1>
+      <p className="content-primary">
+        Доставляем документы, посылки, интернет-заказы икоммерческие грузы между
+        следующими странами: Россия, Узбекистан, Турция, Казахстан, Таджикистан,
+        Кыргызстан, Китай, ОАЭ
+      </p>
+    </div>
+  );
+}
+
+export default App;
